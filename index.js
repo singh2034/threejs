d8d85b99f02d38b8dcba2d45a2c3176b06fa80fb
Aayush <singhaayush2034@gmail.com>
2024-06-26 18:29:34 +0530

    completed

--- index.js
+++ index.js
@@ -1,53 +1,85 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from 'jsm/controls/OrbitControls.js';
+
+import getStarfield from "./src/getStarfield.js";
+import { getFresnelMat } from "./src/getFresnelMat.js";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
-
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, w / h, 0.1, 1000);
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ antialias: true });
-
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
+// THREE.ColorManagement.enabled = true;
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.outputColorSpace = THREE.LinearSRGBColorSpace;
 
-const fov = 75;
-const aspect = w / h;
-const near = 0.1;
-const far = 10;
-
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
-const scene = new THREE.Scene();
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.03;
-
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshStandardMaterial({
-  color: 0xffffff,
-  flatShading: true,
+const earthGroup = new THREE.Group();
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+scene.add(earthGroup);
+new OrbitControls(camera, renderer.domElement);
+const detail = 12;
+const loader = new THREE.TextureLoader();
+const geometry = new THREE.IcosahedronGeometry(1, detail);
+const material = new THREE.MeshPhongMaterial({
+  map: loader.load("./textures/earth-day.jpg"),
+  specularMap: loader.load("./textures/earth-spec.jpg"),
+  bumpMap: loader.load("./textures/earth-bump.jpg"),
+  bumpScale: 0.04,
 });
-const mesh = new THREE.Mesh(geo, mat);
-scene.add(mesh);
+// material.map.colorSpace = THREE.SRGBColorSpace;
+const earthMesh = new THREE.Mesh(geometry, material);
+earthGroup.add(earthMesh);
 
-const wireMat = new THREE.MeshBasicMaterial({
-  color: 0xff9f7f,
-  wireframe: true,
+const lightsMat = new THREE.MeshBasicMaterial({
+  map: loader.load("./textures/earth-night.jpg"),
+  blending: THREE.AdditiveBlending,
 });
-const wireMesh = new THREE.Mesh(geo, wireMat);
-wireMesh.scale.setScalar(1.001);
-mesh.add(wireMesh);
+const lightsMesh = new THREE.Mesh(geometry, lightsMat);
+earthGroup.add(lightsMesh);
 
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0x000000);
-scene.add(hemiLight);
+const cloudsMat = new THREE.MeshStandardMaterial({
+  map: loader.load("./textures/earth-clouds.jpg"),
+  transparent: true,
+  opacity: 0.8,
+  blending: THREE.AdditiveBlending,
+  alphaMap: loader.load("./textures/earth-cloud-map.jpg"),
+  // alphaTest: 0.3,
+});
+const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
+cloudsMesh.scale.setScalar(1.003);
+earthGroup.add(cloudsMesh);
 
-function animate(t = 0) {
-  // console.log(t)
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry, fresnelMat);
+glowMesh.scale.setScalar(1.01);
+earthGroup.add(glowMesh);
+
+const stars = getStarfield({numStars: 2000});
+scene.add(stars);
+
+const sunLight = new THREE.DirectionalLight(0xffffff, 2.0);
+sunLight.position.set(-2, 0.5, 1.5);
+scene.add(sunLight);
+
+function animate() {
   requestAnimationFrame(animate);
-  // mesh.rotation.x = t * 0.0001;
-  // mesh.rotation.y = t * 0.0001;
-  // mesh.scale.setScalar(Math.cos(t * 0.001) * 1.0);
+
+  earthMesh.rotation.y += 0.002;
+  lightsMesh.rotation.y += 0.002;
+  cloudsMesh.rotation.y += 0.0023;
+  glowMesh.rotation.y += 0.002;
+  stars.rotation.y -= 0.0002;
   renderer.render(scene, camera);
-  controls.update();
 }
+
 animate();
+
+function handleWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
